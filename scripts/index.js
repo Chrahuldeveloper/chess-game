5d734b8a441929240b00e5282124b88d65f14039
Chrahuldeveloper <chrahulofficial@gmail.com>
2026-06-10 00:53:49 +0530

    dn

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,86 @@
+const board = document.getElementById("board");
+
+const pieces = [
+    "♜","♞","♝","♛","♚","♝","♞","♜",
+    "♟","♟","♟","♟","♟","♟","♟","♟",
+    "","","","","","","","",
+    "","","","","","","","",
+    "","","","","","","","",
+    "","","","","","","","",
+    "♙","♙","♙","♙","♙","♙","♙","♙",
+    "♖","♘","♗","♕","♔","♗","♘","♖"
+];
+
+for (let i = 0; i < 64; i++) {
+    const square = document.createElement("div");
+    const isDark = (Math.floor(i / 8) + i) % 2;
+
+    square.dataset.index = i
+
+    square.className = `
+        ${isDark ? "dark-square" : "light-square"}
+        relative flex items-center justify-center text-5xl
+    `;
+
+    if (pieces[i]) {
+        const piece = document.createElement("span");
+        piece.className = "piece-label";
+        piece.textContent = pieces[i];
+        square.appendChild(piece);
+    }
+
+    board.appendChild(square);
+}
+
+
 let selectedSquare = null;
-const board = document.querySelector('.grid');
 
-board.addEventListener('click', (e) => {
-    const square = e.target.closest('[class*="light-square"], [class*="dark-square"]');
-    console.log(square)
+function renderBoard() {
+    board.innerHTML = "";
+
+    for (let i = 0; i < 64; i++) {
+        const square = document.createElement("div");
+        const isDark = (Math.floor(i / 8) + i) % 2;
+
+        square.dataset.index = i;
+
+        square.className = `
+            ${isDark ? "dark-square" : "light-square"}
+            relative flex items-center justify-center text-5xl
+        `;
+
+        if (pieces[i]) {
+            const piece = document.createElement("span");
+            piece.className = "piece-label";
+            piece.textContent = pieces[i];
+            square.appendChild(piece);
+        }
+
+        board.appendChild(square);
+    }
+}
+
+board.addEventListener("click", (e) => {
+    const square = e.target.closest(".light-square, .dark-square");
     if (!square) return;
-    move(square)
+
+    const index = Number(square.dataset.index);
+
+    if(selectedSquare === null){
+        selectedSquare = square
+        square.classList.add("border-2", "border-green-500");
+        return;
+    }
+
+    let from = Number(selectedSquare.dataset.index)
+    let to = index
+
+    pieces[to] = pieces[from]
+    pieces[from] = ""
+
+    renderBoard()
+
+    selectedSquare.classList.remove("border-2", "border-green-500");
+    selectedSquare = null;
+
 });
-
-const move = (square) => {
-    const piece = square.textContent.trim();
-    if (!selectedSquare) {
-        selectedSquare = square;
-        square.style.outline = '4px solid yellow';
-        square.style.outlineOffset = '-4px';
-
-    } else {
-        square.textContent = selectedSquare.textContent;
-        selectedSquare.textContent = '';
-        clearSelection()
-    }
-
-}
-
-function clearSelection() {
-    if (selectedSquare) {
-        selectedSquare.style.outline = '';
-        selectedSquare.style.outlineOffset = '';
-        selectedSquare = null;
-    }
-}
